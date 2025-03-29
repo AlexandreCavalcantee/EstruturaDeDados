5cba2900dcd98c3743c0f442e807f8c6d07d57fb
Alexandre Cavalcante <alexandrecavalcante.contato@gmail.com>
2025-03-29 17:19:29 -0300

    oioi

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int[] vetorzao = new int[10];
+        vetorzao[0] = 5;
+        vetorzao[1] = 10;
+        vetorzao[2] = 15;
+        vetorzao[3] = 20;
+        vetorzao[4] = 25;
+        vetorzao[5] = 30;
+        vetorzao[6] = 35;
+        vetorzao[7] = 40;
+        vetorzao[8] = 45;
+        vetorzao[9] = 50;
+
+        System.out.println("Printing Array: ");
+
+        for(int i = 0; i < vetorzao.length; i++) {
+            System.out.println(vetorzao[i]);
+        }
+    }
+}
---
+++ src/Node.java
@@ -0,0 +1,17 @@
+public class Node {
+    private String valor;
+    private Node proximo;
+
+    public String getValor() {
+        return valor;
+    }
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+    public Node getProximo() {
+        return proximo;
+    }
+    public void setProximo(Node proximo) {
+        this.proximo = proximo;
+    }
+}
