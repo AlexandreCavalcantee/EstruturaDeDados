fcf7f0071d935a135c5bea6a7a3215afee8a8686
Alexandre Cavalcante <alexandrecavalcante.contato@gmail.com>
2025-03-31 22:59:55 -0300

    gty

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         vetorzao[8] = 45;
         vetorzao[9] = 599;
 
-        System.out.println("Printing Arrayayay (: ");
+        System.out.println("Printing Arrayaya (; ");
 
         for(int i = 0; i < vetorzao.length; i++) {
             System.out.println(vetorzao[i]);
