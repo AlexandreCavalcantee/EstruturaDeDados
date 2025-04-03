30a47adc47ece605f6feafeef0b491c7502e49c0
Alexandre Cavalcante <alexandrecavalcante.contato@gmail.com>
2025-04-02 23:35:07 -0300

    wow

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         vetorzao[1] = 10;
         vetorzao[2] = 15;
         vetorzao[3] = 222;
-        vetorzao[4] = 25;
+        vetorzao[4] = 29;
         vetorzao[5] = 30;
         vetorzao[6] = 35;
         vetorzao[7] = 40;
