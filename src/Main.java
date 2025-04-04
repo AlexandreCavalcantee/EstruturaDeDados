e48c12085e6f01a1065a90fc279e8ba71e476764
Alexandre Cavalcante <alexandrecavalcante.contato@gmail.com>
2025-04-03 23:31:44 -0300

    dsad

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         int[] vetorzao = new int[10];
         vetorzao[0] = 11;
-        vetorzao[1] = 10;
+        vetorzao[1] = 0;
         vetorzao[2] = 15;
         vetorzao[3] = 222;
         vetorzao[4] = 29;
