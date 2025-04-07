5f0363be559edeaaf57618c7d8ea8d46d127c5f7
Alexandre Cavalcante <alexandrecavalcante.contato@gmail.com>
2025-04-07 10:18:20 -0300

    laele

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         vetorzao[2] = 15;
         vetorzao[3] = 222;
         vetorzao[4] = 2;
-        vetorzao[5] = 3434;
+        vetorzao[5] = 34;
         vetorzao[6] = 4;
         vetorzao[7] = 420;
         vetorzao[8] = 45;
