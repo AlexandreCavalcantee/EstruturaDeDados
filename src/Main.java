65174c79295b61825ab35376ab3ceab103cabec5
Alexandre Cavalcante <alexandrecavalcante.contato@gmail.com>
2025-04-23 21:41:02 -0300

    djadsad

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         vetorzao[4] = 23224334;
         vetorzao[5] = 341212;
         vetorzao[6] = 443434342;
-        vetorzao[7] = 4033;
+        vetorzao[7] = 4033353;
         vetorzao[8] = 432121225;
         vetorzao[9] = 9211829;
 
